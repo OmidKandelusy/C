871a5cb91e8f8dbf72c875d89c1bfa483401256e
Omid Kandelusy <omidkandelusy@gmail.com>
2026-04-01 20:30:53 +0200

    feat: adds an initial version of log_dump subsys

---
+++ subsystems/log_dump/demo.c
@@ -0,0 +1,18 @@
+/** standard c header files */
+#include "logdump.h"
+
+
+int main(void){
+
+    log_dump_init();
+
+    int counter = 0;
+    for (int i=0; i<5; i++){
+        counter++;
+        log_inf("this is the %d-th test message \n\r", counter);
+    }
+
+    log_dump();
+
+    return 0;
+}
